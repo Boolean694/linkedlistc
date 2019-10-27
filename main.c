1d2fe40daea08197d151c248f18eea4d8bc0ae89
Julian Wong <jwong01@stuy.edu>
2019-10-27 11:21:28 -0400

    remove useless comments from main

--- main.c
+++ main.c
@@ -4,25 +4,6 @@
 #include "headers.h"
 
 int main() {
-/*  struct node *no0 = (struct node *)malloc(sizeof(struct node));
-  no0->i = 3;
-  char *name0 = "no0";
-  no0->name = name0;
-  struct node *no1 = (struct node *)malloc(sizeof(struct node));
-  no1->i = 5;
-  char *name1 = "no1";
-  no1->name = name1;
-  struct node *no2 = (struct node *)malloc(sizeof(struct node));
-  no2->i = 7;
-  char *name2 = "no2";
-  no2->name = name2;
-  struct node *p1 = no1;
-  no0->next = p1;
-  struct node *p0 = no0;
-  struct node *p2 = no2;
-  no1->next = p2;
-  no2->next = NULL;
-  */
   struct node *begin = (struct node *)malloc(sizeof(struct node));
   begin = NULL;
   printf("empty: ");
@@ -35,15 +16,6 @@ int main() {
     begin = insert_front(begin, q);
   }
   print_list(begin);
-
-  //no0->next =
-/*  struct node no1;
-  no1.i = 5;
-  struct node no2;
-  no2.i = 7;
-  no2.next = NULL;
-  no0.next = no1;
-  no1.next = no2; */
 
   return 0;
 }
